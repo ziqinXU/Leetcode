b67c52e76335b16eba0501df9bd3edb523946a75
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-04-29 10:31:43 +0200

    Update 1338_Reduce_array_size_to_the_half.cpp

--- Medium/1338_Reduce_array_size_to_the_half.cpp
+++ Medium/1338_Reduce_array_size_to_the_half.cpp
@@ -1,3 +1,6 @@
+///first attempt 22.89%faster, 100%less memory
+//Idea: Save numbers in arrays with their frequency, sort the frequency and remove from the highest frequency, 
+//stop when the remaining part is less or equl to the half.
 class Solution {
 public:
     int minSetSize(vector<int>& arr) {
@@ -8,20 +11,19 @@ public:
     for(int i=0;i<arr.size();i++)
     {
         int j=i+1;
-        while(j<arr.size()&&arr[i]==arr[j])
+        while(j<arr.size()&&arr[i]==arr[j])//count the frequency of a number
         {
             j++;
         }
-        temp[p].first=j-i;
-        temp[p].second=arr[i];
-        //printf("%d %d ssss",j-i,arr[i]);
+        temp[p].first=j-i; //save the frequency
+        temp[p].second=arr[i];//number itself
         i=j-1;
         comparearray.push_back(temp[p]);
     }
-    sort(comparearray.begin(),comparearray.end());
+    sort(comparearray.begin(),comparearray.end());//sort with frequency (first)
     int sum=0;
     int k=comparearray.size()-1;
-    while(arr.size()-sum>arr.size()/2+arr.size()%2)
+    while(arr.size()-sum>arr.size()/2+arr.size()%2)//if the removed part is equal to the half, stop it.
     {
         sum=sum+comparearray[k].first;
         
