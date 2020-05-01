449de7d23fc3f8768a9e345f00fe7c77aca3fcba
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-05-01 18:25:38 +0200

    Update 989_add_to_array-form_of_integer.cpp

--- Easy/989_add_to_array-form_of_integer.cpp
+++ Easy/989_add_to_array-form_of_integer.cpp
@@ -1,16 +1,19 @@
+///first attempt 27.96%faster, 9.09%less memory
+//Idea:reverse A and K,add the number in each position of A and K, and count=1 if it is bigger or equal to 10, reverse
+//the resulting array again.
 class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& A, int K) {
         vector<int>Kvec;
         vector<int>returnarray;
-        while(K/10!=0)
+        while(K/10!=0)//convert K into array;
         {
             Kvec.push_back(K%10);
             K=K/10;
         }
         Kvec.push_back(K%10);
         int temp;
-        for(int i=0;i<A.size()/2;i++)
+        for(int i=0;i<A.size()/2;i++)//reverse array A
         {
             temp=A[i];
             A[i]=A[A.size()-1-i];
@@ -18,7 +21,7 @@ public:
         }
         
         int count=0;
-        if(Kvec.size()<A.size())
+        if(Kvec.size()<A.size())//add numbers from A and K together, count 1 if bigger or equal to 10
         {
             for(int i=0;i<Kvec.size();i++)
             {
@@ -29,7 +32,7 @@ public:
                 count=0;
             }
 
-            for(int i=Kvec.size();i<A.size();i++)
+            for(int i=Kvec.size();i<A.size();i++)//add the longer array
             {
                 returnarray.push_back((A[i]+count)%10);
                 if((A[i]+count)/10==1)
@@ -40,10 +43,10 @@ public:
                 
                 
             }
-            if(count==1)
+            if(count==1)//if the last position has a carry, add 1.
             returnarray.push_back(1);
         }
-        else
+        else//same as above ,only differs from the length of array
         {
             for(int i=0;i<A.size();i++)
             {
@@ -69,7 +72,7 @@ public:
             if(count==1)
             returnarray.push_back(1);
         }
-        for(int i=0;i<returnarray.size()/2;i++)
+        for(int i=0;i<returnarray.size()/2;i++)//reverse the resulting array again
         {
             temp=returnarray[i];
             returnarray[i]=returnarray[returnarray.size()-1-i];
