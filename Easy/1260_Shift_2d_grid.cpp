69376cd3b3ce926de920d99c340dade26f868635
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-05-02 09:47:26 +0200

    Update 1260_Shift_2d_grid.cpp

--- Easy/1260_Shift_2d_grid.cpp
+++ Easy/1260_Shift_2d_grid.cpp
@@ -1,9 +1,11 @@
+///first attempt 41.67%faster, 100%less memory
+//Idea:Move first k mod (size_of_grid) elements to the back
 class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
     vector<int>intarray;
     vector<vector<int>>returnarray;
-    for(int i=0;i<grid.size();i++)
+    for(int i=0;i<grid.size();i++)//convert 2D array into 1D
     {
         for(int j=0;j<grid[i].size();j++)
         {
@@ -11,9 +13,9 @@ public:
         }
     }
     int p=intarray.size()-1;
-    int m=k%(grid.size()*grid[0].size());
+    int m=k%(grid.size()*grid[0].size());//take mod
     vector<int>lastpart;
-    while(m!=0)
+    while(m!=0)//save first k mod (girdsize) elements into a temp vector
     {
         lastpart.push_back(intarray[p]);
         p--;
@@ -23,7 +25,7 @@ public:
     int q=0;
     int r=lastpart.size()-1;
     vector<int>temp;
-    for(int i=0;i<grid.size();i++)
+    for(int i=0;i<grid.size();i++)//convert 1D array again into 2D, insert the temp vector first and then the rest
     {
         for(int j=0;j<grid[i].size();j++)
         {
