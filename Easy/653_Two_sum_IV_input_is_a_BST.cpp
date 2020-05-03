d7bb4bf9385ed56fc0950ae953436c7d6dae9478
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-05-03 10:42:02 +0200

    Update 653_Two_sum_IV_input_is_a_BST.cpp

--- Easy/653_Two_sum_IV_input_is_a_BST.cpp
+++ Easy/653_Two_sum_IV_input_is_a_BST.cpp
@@ -1,3 +1,5 @@
+///first attempt 15.86%faster, 33.33%less memory
+//Idea:in-order, already sorted, use two pointers to check the sum
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -10,7 +12,7 @@
 class Solution {
 public:
     vector<int>intarray;
-    void checkarray(TreeNode* root)
+    void checkarray(TreeNode* root)//in order recursive, save in an int array
     {
         if(root)
         {
@@ -24,18 +26,18 @@ public:
         int i=0,j=intarray.size()-1;
         while(i<j)
         {
-            if(intarray[i]+intarray[j]==k)
+            if(intarray[i]+intarray[j]==k)//if found, return true
             return true;
-            if(intarray[i]+intarray[j]<k)
+            if(intarray[i]+intarray[j]<k)//if lower than the sum, left pointer move one more right
             {
                 i++;
             }
-            if(intarray[i]+intarray[j]>k)
+            if(intarray[i]+intarray[j]>k)//if bigger than the sum, right pointer move one more left
             {
                 j--;
             }
         }
-        return false;
+        return false;//not found
 
     }
 };
