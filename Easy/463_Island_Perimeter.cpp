3a29ec7830545bc9e7ad8d1d7714d7f1838329a9
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-05-04 10:48:05 +0200

    Update 463_Island_Perimeter.cpp

--- Easy/463_Island_Perimeter.cpp
+++ Easy/463_Island_Perimeter.cpp
@@ -1,6 +1,9 @@
+///first attempt 10.83%faster, 5.26%less memory
+//Idea:Check the four cells around the current cell, if the current one is 1 and the side number of current cell
+//is 4-number of cells around current one.
 class Solution {
 public:
-    int surrounding(int r,int c, int i, int j,vector<vector<int>>& grid)
+    int surrounding(int r,int c, int i, int j,vector<vector<int>>& grid)//check the side number
     {
         int side=0;
         if(i-1>=0)
@@ -32,7 +35,7 @@ public:
     {
         for(int j=0;j<grid[i].size();j++)
         {
-            if(grid[i][j]==1)
+            if(grid[i][j]==1)//if the current cell is equal to one, check its surroundings.
             {
                 count=count+surrounding(grid.size(),grid[i].size(),i,j,grid);
             }
