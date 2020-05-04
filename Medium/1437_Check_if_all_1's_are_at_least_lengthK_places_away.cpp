5e91f795997efc1ea38e1a71e960d5ee80c4658f
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-05-04 10:52:30 +0200

    Update 1437_Check_if_all_1's_are_at_least_lengthK_places_away.cpp

--- Medium/1437_Check_if_all_1's_are_at_least_lengthK_places_away.cpp
+++ Medium/1437_Check_if_all_1's_are_at_least_lengthK_places_away.cpp
@@ -1,8 +1,10 @@
+///first attempt 100%faster, 100%less memory
+//Idea:Use vector to record position of each 1, and check distance between each 1.
 class Solution {
 public:
     bool kLengthApart(vector<int>& nums, int k) {
     vector<int>pos;
-    for(int i=0;i<nums.size();i++)
+    for(int i=0;i<nums.size();i++)//record position of ones.
     {
         if(nums[i]==1)
         pos.push_back(i);
@@ -10,7 +12,7 @@ public:
     if(pos.size()>=1)
     {
         
-        for(int i=0;i<pos.size()-1;i++)
+        for(int i=0;i<pos.size()-1;i++)//check the distances
         {
             
             if(pos[i+1]-pos[i]<=k)
