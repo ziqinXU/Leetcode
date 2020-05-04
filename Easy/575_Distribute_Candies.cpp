dbd68a9bc6eee74cb1f8ab1546068580c9c17d9e
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-05-04 10:42:37 +0200

    Update 575_Distribute_Candies.cpp

--- Easy/575_Distribute_Candies.cpp
+++ Easy/575_Distribute_Candies.cpp
@@ -1,12 +1,17 @@
+///first attempt 19.58%faster, 20%less memory
+//Idea:
 class Solution {
 public:
     int distributeCandies(vector<int>& candies) {
     map<int,int>hashmap;
     for(int i=0;i<candies.size();i++)
     {
-        hashmap[candies[i]]++;
+        hashmap[candies[i]]++;//count the number of candies in each category
     }
-    if(candies.size()/2<=hashmap.size())
+   /*if the category number is bigger or equal to the half of whole candies, 
+   the sister can get maximum half of it. Otherwise, all sorts of candies.
+   */
+    if(candies.size()/2<=hashmap.size()) 
     return candies.size()/2;
     else
     return hashmap.size();
