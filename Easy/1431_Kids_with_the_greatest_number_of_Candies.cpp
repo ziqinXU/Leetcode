d49044ff55ef5d27d08a53c0969b38656cc2f00f
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-05-04 10:57:19 +0200

    Update 1431_Kids_with_the_greatest_number_of_Candies.cpp

--- Easy/1431_Kids_with_the_greatest_number_of_Candies.cpp
+++ Easy/1431_Kids_with_the_greatest_number_of_Candies.cpp
@@ -1,9 +1,12 @@
+///first attempt 100%faster, 100%less memory
+//Idea:Sort the array with number of candies for each kid, compare the maximum candy and the current candy, if 
+//being added the extra candy number, the sum exeeds or equal to the maximum number, return true, otherwise, false.
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
     vector<pair<int,int>>sortedcandies;
     pair<int,int>temp[100];
-    for(int i=0;i<candies.size();i++)
+    for(int i=0;i<candies.size();i++)//sort the array with candy number
     {
         temp[i].first=candies[i];
         temp[i].second=i;
@@ -14,19 +17,15 @@ public:
     {
         returnarray.push_back(false);
     }
-
     sort(sortedcandies.begin(),sortedcandies.end());
     int current=sortedcandies.size()-1;
-    
+    //check whether with extra candies, the kid can have the same or more candies than the maximum, if so, return true;
     while(current>=0&&sortedcandies[sortedcandies.size()-1].first-sortedcandies[current].first<=extraCandies)
-    {
-       
+    { 
         returnarray[sortedcandies[current].second]=true;
         current--;
-        
     }
     
     return returnarray;
-
     }
 };
