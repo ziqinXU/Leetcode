4a5d7ba7bb4d26adce49986eba912f1883f4ceba
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-05-08 22:31:34 +0200

    Update 624_Maximum_Distance_in_Arrays.cpp

--- Easy/624_Maximum_Distance_in_Arrays.cpp
+++ Easy/624_Maximum_Distance_in_Arrays.cpp
@@ -1,3 +1,5 @@
+///first attempt 30.1%faster, 100%less memory
+//Idea:compare select past min, past max, current min, current max, calculate the maximum distance and save min and max from those four numbers
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& arrays) {
@@ -13,11 +15,11 @@ public:
         current=arrays[i][arrays[i].size()-1]-temparray[0];
         else
         current=temparray[1]-arrays[i][0];
-        if(arrays[i][0]<temparray[0])
+        if(arrays[i][0]<temparray[0])//min 
         temparray[0]=arrays[i][0];
-        if(arrays[i][arrays[i].size()-1]>temparray[1])
+        if(arrays[i][arrays[i].size()-1]>temparray[1])//max
         temparray[1]=arrays[i][arrays[i].size()-1];
-        if(current>temparray[2])
+        if(current>temparray[2])//distance
         temparray[2]=current;
             
     }
