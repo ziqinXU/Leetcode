57b0b2204e9413128426b50f57e48cd01cc62fcf
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-05-09 11:34:19 +0200

    Update 1433_Check_if_a_string_can_break_another_string.cpp

--- Medium/1433_Check_if_a_string_can_break_another_string.cpp
+++ Medium/1433_Check_if_a_string_can_break_another_string.cpp
@@ -1,23 +1,23 @@
+///first attempt 50.53%faster, 100%less memory
+//Idea:sort two strings, check if the dictionary order of all position in s1 is greater than s2, and vice versa.
 class Solution {
 public:
     bool checkIfCanBreak(string s1, string s2) {
-        sort(s1.begin(),s1.end());
+        sort(s1.begin(),s1.end());//sort s1 and s2
         sort(s2.begin(),s2.end());
         int count1=0,count2=0;
-        for(int i=0;i<s1.size();i++)
+        for(int i=0;i<s1.size();i++)//check if the dictionary order of all position in s1 is greater than s2
         {
             if(s1[i]>s2[i])
             {
                 count1++;
-                //std::cout<<count<<endl;
             }
-            if(s1[i]<s2[i])
+            if(s1[i]<s2[i])//check if the dictionary order of all position in s2 is greater than s1
             {
                 count2++;
             }
         }
-        //std::cout<<count1<<endl;
-        if(count1==0||count2==0)
+        if(count1==0||count2==0)//all in s1 is greater than s2 or s2 is greater than s1 or all alpabets are the same, return true
         return true;
         else
         return false;
