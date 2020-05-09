df910101f131d78396d53d263866d49cd439bbcb
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-05-09 11:21:07 +0200

    Update 1060_Missing_elements_in_sorted_array.cpp

--- Medium/1060_Missing_elements_in_sorted_array.cpp
+++ Medium/1060_Missing_elements_in_sorted_array.cpp
@@ -1,3 +1,6 @@
+///first attempt 38.89%faster, 100%less memory
+//Idea: if there is a gap between two number,substrct the missing number from k and until no k left. If there is still missing number
+//at the end, add the corresponding number.
 class Solution {
 public:
     int missingElement(vector<int>& nums, int k) {
@@ -5,19 +8,19 @@ public:
         for(int i=0;i<nums.size()-1;i++)
         {
             
-            if(nums[i+1]-nums[i]>1)
+            if(nums[i+1]-nums[i]>1)//if there is a gap
             {
-                rest=k-(nums[i+1]-nums[i]-1);
+                rest=k-(nums[i+1]-nums[i]-1);//check remaining number
                 
             }
             
-            if(rest<=0)
+            if(rest<=0)//already within the gap
             return nums[i]+k;
-            k=k-(nums[i+1]-nums[i]-1);
+            k=k-(nums[i+1]-nums[i]-1);//otherwise check remining number
         }
         
         if(k>0)
-        return nums[nums.size()-1]+k;
+        return nums[nums.size()-1]+k;//exceed the array
         return 0;
     }
 };
