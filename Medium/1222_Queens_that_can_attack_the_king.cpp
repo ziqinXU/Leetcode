8f30597c7559deffb14c451bc4c1430dcbd70134
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-05-10 15:54:19 +0200

    Add files via upload

---
+++ Medium/1222_Queens_that_can_attack_the_king.cpp
@@ -0,0 +1,132 @@
+class Solution {
+public:
+    vector<vector<int>> queensAttacktheKing(vector<vector<int>>& queens, vector<int>& king) {
+        vector<vector<int>> returnarray;
+        vector<int>temp;
+        int intarray[9][9];
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                intarray[i][j]=0;
+            }
+        }
+        for(int i=0;i<queens.size();i++)
+        {
+            intarray[queens[i][0]][queens[i][1]]=1;
+        }
+        int i=king[0]-1;
+        while(i>=0)
+        {
+            if(intarray[i][king[1]]==1)
+            {
+                temp.push_back(i);
+                temp.push_back(king[1]);
+                returnarray.push_back(temp);
+                temp.clear();
+                break;
+            }
+            i--;
+        }
+        i=king[0]+1;
+        while(i<9)
+        {
+            if(intarray[i][king[1]]==1)
+            {
+                temp.push_back(i);
+                temp.push_back(king[1]);
+                returnarray.push_back(temp);
+                temp.clear();
+                break;
+            }
+            i++;
+        }
+        int j=king[1]-1;
+        while(j>=0)
+        {
+            if(intarray[king[0]][j]==1)
+            {
+                temp.push_back(king[0]);
+                temp.push_back(j);
+                returnarray.push_back(temp);
+                temp.clear();
+                break;
+            }
+            j--;
+        }
+        j=king[1]+1;
+        while(j<9)
+        {
+            if(intarray[king[0]][j]==1)
+            {
+                temp.push_back(king[0]);
+                temp.push_back(j);
+                returnarray.push_back(temp);
+                temp.clear();
+                break;
+            }
+            j++;
+        }
+        i=king[0]-1;
+        j=king[1]-1;
+        while(i>=0&&j>=0)
+        {
+            if(intarray[i][j]==1)
+            {
+                temp.push_back(i);
+                temp.push_back(j);
+                returnarray.push_back(temp);
+                temp.clear();
+                break;
+            }
+            i--;
+            j--;
+        }
+        i=king[0]+1;
+        j=king[1]+1;
+        while(i<9&&j<9)
+        {
+            if(intarray[i][j]==1)
+            {
+                temp.push_back(i);
+                temp.push_back(j);
+                returnarray.push_back(temp);
+                temp.clear();
+                break;
+            }
+            i++;
+            j++;
+        }
+        i=king[0]-1;
+        j=king[1]+1;
+        while(i>=0&&j<9)
+        {
+            if(intarray[i][j]==1)
+            {
+                temp.push_back(i);
+                temp.push_back(j);
+                returnarray.push_back(temp);
+                temp.clear();
+                break;
+            }
+            i--;
+            j++;
+        }
+        i=king[0]+1;
+        j=king[1]-1;
+        while(j>=0&&i<9)
+        {
+            if(intarray[i][j]==1)
+            {
+                temp.push_back(i);
+                temp.push_back(j);
+                returnarray.push_back(temp);
+                temp.clear();
+                break;
+            }
+            i++;
+            j--;
+        }
+        return returnarray;
+    }
+};
