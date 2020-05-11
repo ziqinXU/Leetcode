583ade99ca39da011a14471a36d920e820dd366c
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-05-11 15:39:13 +0200

    Update 303_Range_sum_query_immutable.cpp

--- Easy/303_Range_sum_query_immutable.cpp
+++ Easy/303_Range_sum_query_immutable.cpp
@@ -1,3 +1,5 @@
+///first attempt 34.8%faster, 100%less memory
+//Idea:Use prefix to save temp sum.
 class NumArray {
 private:
     vector<int> pre;
@@ -6,14 +8,14 @@ public:
     NumArray(vector<int>& nums) {
         pre.resize(nums.size()+1,0);
         pre.push_back(0);
-        for(int i=0;i<nums.size();i++)
+        for(int i=0;i<nums.size();i++)//save prefix
         {
             pre[i+1]=pre[i]+nums[i];
         }
         
     }
     
-    int sumRange(int i, int j) {
+    int sumRange(int i, int j) {//substract
         return pre[j+1]-pre[i];
     }
 };
