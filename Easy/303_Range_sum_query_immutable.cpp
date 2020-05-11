0488571a1c391f9763b0bbfcbc84069ac199e639
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-05-11 15:37:58 +0200

    Add files via upload

---
+++ Easy/303_Range_sum_query_immutable.cpp
@@ -0,0 +1,25 @@
+class NumArray {
+private:
+    vector<int> pre;
+public:
+    
+    NumArray(vector<int>& nums) {
+        pre.resize(nums.size()+1,0);
+        pre.push_back(0);
+        for(int i=0;i<nums.size();i++)
+        {
+            pre[i+1]=pre[i]+nums[i];
+        }
+        
+    }
+    
+    int sumRange(int i, int j) {
+        return pre[j+1]-pre[i];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(i,j);
+ */
