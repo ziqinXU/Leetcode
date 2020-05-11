1fe4d4d4267686e3c9bc35ebfb8f49cf52b19d5d
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-05-11 11:26:27 +0200

    Update 5404_Build_an_array_with_stack_operations.cpp

--- Easy/5404_Build_an_array_with_stack_operations.cpp
+++ Easy/5404_Build_an_array_with_stack_operations.cpp
@@ -1,3 +1,5 @@
+///first attempt 100%faster, 100%less memory
+//Idea:If the number is not in the array, add push and pop, if it is in, add push
 class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
@@ -5,7 +7,7 @@ public:
     int i=0,p=1;
     while(1)
     {
-        if(target[i]-p!=0)
+        if(target[i]-p!=0)//not in the array, push first and then pop
         {
             returnarray.push_back("Push");
             returnarray.push_back("Pop");
@@ -13,7 +15,7 @@ public:
         }
         else
         {
-            returnarray.push_back("Push");
+            returnarray.push_back("Push");//in the array, push
             i++;
             p++;
         }
