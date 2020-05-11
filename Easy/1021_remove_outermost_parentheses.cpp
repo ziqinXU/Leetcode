b965c187d7c8084f679e63dd020d57fcfcc41e25
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-05-11 11:22:55 +0200

    Update 1021_remove_outermost_parentheses.cpp

--- Easy/1021_remove_outermost_parentheses.cpp
+++ Easy/1021_remove_outermost_parentheses.cpp
@@ -1,3 +1,5 @@
+///first attempt 50.34%faster, 100%less memory
+//Idea:count the left and right parentheses number, if they are the same, add the parentheses inside into the result
 class Solution {
 public:
     string removeOuterParentheses(string S) {
@@ -14,10 +16,10 @@ public:
         {
             right++;
         }
-        if(left==right)
+        if(left==right)//number of left and right are the same
         {
-            returnarray.append(S.substr(start+1,i-start-1));
-            start=i+1;
+            returnarray.append(S.substr(start+1,i-start-1));//add the parentheses inside into the result
+            start=i+1;//start from the new position
         }
     }
     return returnarray;
