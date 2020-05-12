22706da953d8f52839e69f5d39406478a4b04fca
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-05-12 11:29:22 +0200

    Update 702_Search_in_a_sorted_array_of_unknown_size.cpp

--- Medium/702_Search_in_a_sorted_array_of_unknown_size.cpp
+++ Medium/702_Search_in_a_sorted_array_of_unknown_size.cpp
@@ -1,3 +1,6 @@
+///first attempt 27.81%faster, 100%less memory
+//Idea:use binary search algorithm to search the target, but since here the length is not given, we can enrich
+//the range by *2 of the right side each time until target is inside
 /**
  * // This is the ArrayReader's API interface.
  * // You should not implement it, or speculate about its implementation
@@ -11,14 +14,14 @@ class Solution {
 public:
     int search(const ArrayReader& reader, int target) {
         int i=0,j=1;
-        while(reader.get(j)<target)
+        while(reader.get(j)<target)//enrich the range until the target is inside
         {
             j=j*2;
         }
   
         int left=i,right=j;
         int mid;
-        while(left<=right)
+        while(left<=right)//use binary search to find the target
         {
             mid=(left+right)/2;
            
