24a818d56922de008f14c18d8c078efb3142793b
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-05-16 20:39:30 +0200

    Update 1344_Angle_between_hands_of_a_clock.cpp

--- Medium/1344_Angle_between_hands_of_a_clock.cpp
+++ Medium/1344_Angle_between_hands_of_a_clock.cpp
@@ -1,8 +1,10 @@
+///first attempt %faster, %less memory
+//Idea:Calculate the angle mathematically with two different situations.
 class Solution {
 public:
     double angleClock(int hour, int minutes) {
         double number;
-        if(hour==12)
+        if(hour==12)//two special occasions
         hour=0;
         if(minutes==0)
         {
@@ -12,7 +14,7 @@ public:
             return (double)hour/12*360;
         }
         
-        if((int)((double)minutes/60*12)>hour)
+        if((int)((double)minutes/60*12)>hour)//if minutes is larger than hour number
         {
             number=((double)minutes/60*12-((double)hour+1)+1-(double)minutes/60)*360/12;
             if(number>180)
