8ce6d1aef8a84b173502c1b102a1e32de30c9b29
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-05-18 17:40:41 +0200

    Update 985_Sum_of_Even_numbers_after_queries.cpp

--- Easy/985_Sum_of_Even_numbers_after_queries.cpp
+++ Easy/985_Sum_of_Even_numbers_after_queries.cpp
@@ -1,9 +1,11 @@
+///first attempt 30.75%faster, 25%less memory
+//Idea:Add the sum of original array A first and then check the odevity of the number in A and in queries and classify them into four cases.
 class Solution {
 public:
     vector<int> sumEvenAfterQueries(vector<int>& A, vector<vector<int>>& queries) {
     int sum=0;
     vector<int>returnarray;
-    for(int i=0;i<A.size();i++)
+    for(int i=0;i<A.size();i++)//add all elements in A together
     {
         if(A[i]%2==0)
         sum=sum+A[i];
@@ -12,32 +14,32 @@ public:
     for(int i=0;i<queries.size();i++)
     {
        
-        if(abs(queries[i][0])%2==0&&abs(A[queries[i][1]])%2==0)
+        if(abs(queries[i][0])%2==0&&abs(A[queries[i][1]])%2==0)//if both of them are even,add the number in queries
         {
             sum=sum+queries[i][0];
             returnarray.push_back(sum);
             
         }
-        else if(abs(queries[i][0])%2==1&&abs(A[queries[i][1]])%2==1)
+        else if(abs(queries[i][0])%2==1&&abs(A[queries[i][1]])%2==1)//if both of them are odd,add both
         {
             sum=sum+queries[i][0]+A[queries[i][1]];
             
             returnarray.push_back(sum);
             
         }
-        else if(abs(queries[i][0])%2==0&&abs(A[queries[i][1]]%2)==1)
+        else if(abs(queries[i][0])%2==0&&abs(A[queries[i][1]]%2)==1)//even+odd=even,keep the same
         {
             sum=sum;
             returnarray.push_back(sum);
             
         }
-        else if(abs(queries[i][0])%2==1&&abs(A[queries[i][1]]%2)==0)
+        else if(abs(queries[i][0])%2==1&&abs(A[queries[i][1]]%2)==0)//even+odd=even,the number in A should be removed
         {
             sum=sum-A[queries[i][1]];
             returnarray.push_back(sum);
            
         }
-         A[queries[i][1]]=A[queries[i][1]]+queries[i][0];
+         A[queries[i][1]]=A[queries[i][1]]+queries[i][0];//do changes
          
     }
     return returnarray;
