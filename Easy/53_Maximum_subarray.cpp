143cc099f891845386f0596d8f3c662563597060
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-05-23 17:23:50 +0200

    Update 53_Maximum_subarray.cpp

--- Easy/53_Maximum_subarray.cpp
+++ Easy/53_Maximum_subarray.cpp
@@ -1,3 +1,5 @@
+///first attempt 97.44%faster, 100%less memory
+//Idea:Use variable:currentsum,previoussum,max. If previoussum is less than 0, let it be 0. 
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
@@ -6,11 +8,11 @@ public:
     int previoussum=0;
     for(int i=0;i<nums.size();i++)
     {
-        if(previoussum<0)
+        if(previoussum<0)//if the previous sum is less than 0, let it be zero
         previoussum=0;
         currentsum=previoussum+nums[i];
         previoussum=currentsum;
-        if(currentsum>max)
+        if(currentsum>max)//check the maximum value
         max=currentsum;
     }
     return max;
