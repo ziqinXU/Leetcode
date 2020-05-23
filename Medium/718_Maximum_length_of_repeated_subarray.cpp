d8fb34a22537a5b33990a17793be544b156f8892
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-05-23 17:15:47 +0200

    Update 718_Maximum_length_of_repeated_subarray.cpp

--- Medium/718_Maximum_length_of_repeated_subarray.cpp
+++ Medium/718_Maximum_length_of_repeated_subarray.cpp
@@ -1,6 +1,8 @@
+///first attempt 59.12%faster, 100%less memory
+//Idea:Check the positions where elements in A and in B are the same. Use Dynamic programming to check the diagnal elements
 class Solution {
 public:
-    int findLength(vector<int>& A, vector<int>& B) {
+    int findLength(vector<int>& A, vector<int>& B) {//if elements in A and in B are the same, remember the position
     int matrix[1000][1000]={0};
     int maxlength=0;
     for(int i=0;i<B.size();i++)
@@ -9,17 +11,16 @@ public:
         {
             if(B[i]==A[j])
             matrix[i][j]=1;
-           // printf("%d ",matrix[i][j]);
+         
         }
-       // printf("\n");
+     
     }
-    for(int i=1;i<B.size();i++)
+    for(int i=1;i<B.size();i++)//check the diagnal elements in i-1,j-1 position, if current and last elements are not 0, add 1 
     {
         for(int j=1;j<A.size();j++)
         {
             if(matrix[i-1][j-1]!=0&&matrix[i][j]!=0)
             matrix[i][j]=matrix[i-1][j-1]+1;
-           // printf("%d %d %d    ",matrix[i][j],i,j);
             if(matrix[i][j]>maxlength)
             maxlength=matrix[i][j];
         }
