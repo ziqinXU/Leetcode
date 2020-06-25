9aaeca51fc9cc52578c36fa39a91559cee3578f3
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-06-25 09:42:13 +0200

    Create 344_reverse_string.py

---
+++ Easy_python/344_reverse_string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        i=0
+        while i < int(len(s)/2):
+            temp=s[len(s)-i-1]
+            s[len(s)-i-1]=s[i]
+            s[i]=temp
+            i=i+1
+
+        
