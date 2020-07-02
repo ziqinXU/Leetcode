bcff924a6369859353aad661b00202cc417ffcf1
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-07-02 10:28:35 +0200

    Create 509_fibonacci_number.py

---
+++ Easy_python/509_fibonacci_number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def fib(self, N: int) -> int:
+        if N==0:
+            return 0
+        if N==1:
+            return 1
+        if N!=0 and N!=1:
+            return self.fib(N-1)+self.fib(N-2)
