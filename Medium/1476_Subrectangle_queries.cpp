2997e888c0cd9fde4445c2a21e48c87fd8ff9779
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-09-02 09:46:00 +0200

    Create 1476_Subrectangle_queries.cpp

---
+++ Medium/1476_Subrectangle_queries.cpp
@@ -0,0 +1,31 @@
+class SubrectangleQueries {
+public:
+    
+    SubrectangleQueries(vector<vector<int>>& rectangle) {
+        
+        newrectangle=rectangle;
+    }
+    
+    void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+        for(int i=row1;i<=row2;i++)
+        {
+            for(int j=col1;j<=col2;j++)
+            {
+                newrectangle[i][j]=newValue;
+            }
+        }
+    }
+    
+    int getValue(int row, int col) {
+        return newrectangle[row][col];
+    }
+    private:
+    vector<vector<int>> newrectangle; 
+};
+
+/**
+ * Your SubrectangleQueries object will be instantiated and called as such:
+ * SubrectangleQueries* obj = new SubrectangleQueries(rectangle);
+ * obj->updateSubrectangle(row1,col1,row2,col2,newValue);
+ * int param_2 = obj->getValue(row,col);
+ */
