ea162612244e62bbdfad2e37977b04fbc4269021
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-11-01 09:14:45 +0100

    Update 1602_Find_nearest_right_node_in_binary_tree.py

--- Medium_Python/1602_Find_nearest_right_node_in_binary_tree.py
+++ Medium_Python/1602_Find_nearest_right_node_in_binary_tree.py
@@ -6,41 +6,33 @@
 #         self.right = right
 class Solution:
     def findNearestRightNode(self, root: TreeNode, u: TreeNode) -> TreeNode:
-        res=[]
         def Levelorder(root,flag):
             queue=[]
-            #seen=set()
             queue.append(root)
-            res.append(root.val)
-            if root.val==u.val:
+            if root.val==u.val:#若当前根节点为所找节点，则返回Null
                 return None
             while len(queue)>0:
                 size=len(queue)
-                while size!=0:
+                while size!=0:#层次遍历
                     nodes=queue.pop(0)
                     left=nodes.left
                     right=nodes.right
                     
-                    if left is not None :
+                    if left is not None :#用flag记录是否找到节点，若上一节点为所寻节点，则返回当前节点
                         if flag==1:
                             return left
                         if left.val==u.val:
                             flag=1
                         
                         queue.append(left)
-                        #seen.add(left.val)
-                        res.append(left.val)
-                    
                     if right is not None:
                         if flag==1:
                             return right
                         if right.val==u.val:
                             flag=1
                         queue.append(right)
-                        #seen.add(right.val)
-                        res.append(right.val)
                     size-=1
-                if flag==1:
+                if flag==1:#若遍历完当前层，找到节点，但未返回，则说明右侧节点为Null
                     return None
-            return None
+            return None#遍历完成，未找到右侧节点，返回Null
         return Levelorder(root,0)
