a86926538c90d8c4f0613ced6671e2c1dc7351b7
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-11-10 10:19:31 +0100

    Create 1646_get_maximum_in_generated_array.cpp

---
+++ Easy/1646_get_maximum_in_generated_array.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int getMaximumGenerated(int n) {
+        if(n == 0)
+        {
+            return 0;
+        }
+        if(n == 1)
+        {
+            return 1;
+        }
+        int nums[n+1];
+        nums[0]=0;
+        nums[1]=1;
+        int max=0;
+        for(int i = 2;i <= n;i++)
+        {
+            if(i%2 == 0)
+            {
+                nums[i] = nums[i/2];
+            }
+            else
+            {
+                nums[i] = nums[i/2]+nums[i/2+1];
+            }
+            if(nums[i] > max)
+            {
+                max = nums[i];
+            }
+            //cout<<nums[i]<<endl;
+        }
+        return max;
+    }
+
+};
