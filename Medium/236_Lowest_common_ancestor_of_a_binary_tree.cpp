9cbe4b753ef98c8a4a145f5e3eab31f772ed2c7e
ziqinXU <61913225+ziqinXU@users.noreply.github.com>
2020-12-06 10:57:42 +0100

    Create 236_Lowest_common_ancestor_of_a_binary_tree.cpp

---
+++ Medium/236_Lowest_common_ancestor_of_a_binary_tree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == p || root == q || root == nullptr)
+        {
+            return root;
+        }
+        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
+        if(left == nullptr)
+        return right;
+        if(right == nullptr)
+        return left;
+        return root;//left 和right同时不为空，说明此时的root为所求节点
+        
+    }
+};
